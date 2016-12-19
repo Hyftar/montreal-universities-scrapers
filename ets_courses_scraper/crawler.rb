addcbc3586b4156bbdfe99e5867180706d28b4ea
Simon Landry <simon_landry@ymail.com>
2016-12-19 16:55:05 -0500

    Improve multithread logic

--- ets_courses_scraper/crawler.rb
+++ ets_courses_scraper/crawler.rb
@@ -5,16 +5,14 @@ require 'nokogiri'
 # Gets all the courses from the ÉTS website and outputs them in HTML format
 
 def get_next_uri
-  uri = URI("https://www.etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Cours-par-titre?lettre=#{@l}")
-  @l = @l.next
-  return uri
+  URI("https://www.etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Cours-par-titre?lettre=#{@letters.pop}")
 end
 
 def get_courses(uri)
   Net::HTTP.get(uri)
 end
 
-@l = 'a'
+@letters = ('a'..'z').to_a
 @num_of_threads = 26
 @is_writing = Mutex.new
 
@@ -22,12 +20,11 @@ threads = []
 
 @num_of_threads.times {
   threads << Thread.new {
-    until @l > 'z' do
-      uri = get_next_uri
+    until @letters.empty? do
+      content = get_courses(get_next_uri)
       @is_writing.lock
       File.open('data.html', 'a') { |io|
-        io.puts Nokogiri::HTML(
-          get_courses(uri))
+        io.puts Nokogiri::HTML(content)
             .css('#plc_lt_zoneMain_pageplaceholder_pageplaceholder_lt_zoneContent_pageplaceholder_pageplaceholder_lt_zoneCenter_pageplaceholder_pageplaceholder_lt_zoneCenter_ListeCoursParTitre_GridViewResultats')
             # Why the fuck is this ID so long?!
       }
