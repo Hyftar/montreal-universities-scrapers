d2a60a38f1a16b3da20059e89bd6409fa12df335
Simon Landry <simon_landry@ymail.com>
2016-12-19 16:15:36 -0500

    Add ets courses scraper / crawler

---
+++ ets_courses_scraper/crawler.rb
@@ -0,0 +1,39 @@
+require 'net/https'
+require 'thread'
+require 'nokogiri'
+
+# Gets all the courses from the UdeM website and outputs them in HTML format
+
+def get_next_uri
+  uri = URI("https://www.etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Cours-par-titre?lettre=#{@l}")
+  @l = (@l.ord + 1).chr
+  return uri
+end
+
+def get_courses(uri)
+  Net::HTTP.get(uri)
+end
+
+@l = 'a'
+@num_of_threads = 26
+@is_writing = Mutex.new
+
+threads = []
+
+@num_of_threads.times {
+  threads << Thread.new {
+    until @l.ord > 'z'.ord do
+      uri = get_next_uri
+      @is_writing.lock
+      File.open('data.html', 'a') { |io|
+        io.puts Nokogiri::HTML(
+          get_courses(uri))
+            .css('#plc_lt_zoneMain_pageplaceholder_pageplaceholder_lt_zoneContent_pageplaceholder_pageplaceholder_lt_zoneCenter_pageplaceholder_pageplaceholder_lt_zoneCenter_ListeCoursParTitre_GridViewResultats')
+            # Why the fuck is this ID so long?!
+      }
+      @is_writing.unlock
+    end
+  }
+}
+
+threads.each { |thr| thr.join }
---
+++ ets_courses_scraper/parser.rb
@@ -0,0 +1,72 @@
+require 'nokogiri'
+require 'json'
+require 'parallel'
+require 'net/https'
+
+
+# Parses the data from data.html into JSON format
+# Execute crawler.rb first to generate the data file
+
+class Course
+  attr_accessor :title, :number, :url, :credits, :school
+
+  def initialize(title, number, url, credits)
+    @title = title
+    @number = number
+    @url = "https://www.etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/#{url}"
+    @credits = credits
+    @school = 'École de technologie supérieure'
+  end
+
+  def to_s
+    "#{title}, #{number}, #{credits}, #{url}"
+  end
+
+  def to_hash
+    {
+      "title" => self.title,
+      "url" => self.url,
+      "number" => self.number,
+      "credits" => self.credits,
+      "school" => self.school
+    }
+  end
+end
+
+courses = Nokogiri::HTML(File.open('data.html', 'r').read)
+
+courses_html =
+  courses.css('tr[class="GrilleRowImpair"]') +
+  courses.css('tr[class="GrilleRowPair"]')
+
+
+courses_objects =
+  courses_html
+    .map { |x|
+      Course.new(
+        x.css('td[class="ListeCoursGrilleCol2"]').text.strip,
+        x.css('td[class="ListeCoursGrilleCol1"]').css('a').text,
+        x.css('td[class="ListeCoursGrilleCol1"]').css('a')[0]['href'],
+        0 # Placeholder
+      )
+    }
+
+puts "Number of courses found: #{courses_objects.size}"
+
+Parallel.each(courses_objects, progress: "Doing stuff", in_threads: 20) { |course|
+  page = Net::HTTP.get(URI(course.url))
+  credits_span =
+    Nokogiri::HTML(page)
+      .css('span#plc_lt_zoneMain_pageplaceholder_pageplaceholder_lt_zoneRight_FicheCoursInfosDroite_LabelCredits')
+      .text
+      .match(/(\d+(?:[.,]\d+)?)/)
+
+  course.credits = credits_span ? credits_span.captures.first.to_f : 0
+}
+
+open('courses_fr.json', 'w') { |io|
+  io.puts JSON.pretty_generate(
+    courses_objects
+      .map(&:to_hash)
+  )
+}
