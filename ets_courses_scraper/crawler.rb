cee1d77e28fbf057e5181209bfafa53592ed86d6
Hyftar <simon_landry@ymail.com>
2016-12-22 01:37:29 -0500

    Add more programs and refactor code

--- ets_courses_scraper/crawler.rb
+++ ets_courses_scraper/crawler.rb
@@ -1,36 +1,32 @@
 require 'net/https'
 require 'thread'
 require 'nokogiri'
+require 'parallel'
 
 # Gets all the courses from the ÉTS website and outputs them in HTML format
 
-def get_next_uri
-  URI("https://www.etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Cours-par-titre?lettre=#{@letters.pop}")
-end
 
 def get_courses(uri)
-  Net::HTTP.get(uri)
+  Nokogiri::HTML(Net::HTTP.get(uri))
+    .css('#plc_lt_zoneMain_pageplaceholder_pageplaceholder_lt_zoneContent_pageplaceholder_pageplaceholder_lt_zoneCenter_pageplaceholder_pageplaceholder_lt_zoneCenter_ListeCoursParTitre_GridViewResultats')
 end
 
-@letters = ('a'..'z').to_a
-@num_of_threads = 26
-@is_writing = Mutex.new
+uris = Enumerator.new do |yielder|
+  ('a'..'z').each do |c|
+    yielder << URI("https://www.etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Cours-par-titre?lettre=#{c}")
+    yielder << URI("https://www.etsmtl.ca/Etudiants-actuels/Cycles-sup/Cours-horaires-cycles-sup/Cours-par-titre?lettre=#{c}")
+    yielder << URI("https://www.etsmtl.ca/Etudiants-actuels/Cheminement-univ-techno/Cours-horaires-chem-univ/Cours-par-titre?lettre=#{c}")
+    yielder << URI("https://www.etsmtl.ca/Etudiants-actuels/Certificat-prog-court-1er-cycle/Cours-horaires-1er-cycle/Cours-par-titre?lettre=#{c}")
+  end
+end
 
-threads = []
+is_writing = Mutex.new
 
-@num_of_threads.times {
-  threads << Thread.new {
-    until @letters.empty? do
-      content = get_courses(get_next_uri)
-      @is_writing.lock
-      File.open('data.html', 'a') { |io|
-        io.puts Nokogiri::HTML(content)
-            .css('#plc_lt_zoneMain_pageplaceholder_pageplaceholder_lt_zoneContent_pageplaceholder_pageplaceholder_lt_zoneCenter_pageplaceholder_pageplaceholder_lt_zoneCenter_ListeCoursParTitre_GridViewResultats')
-            # Why the fuck is this ID so long?!
-      }
-      @is_writing.unlock
-    end
-  }
-}
-
-threads.each { |thr| thr.join }
+Parallel.each(uris, progress: "Getting courses HTML pages", in_threads: 30) do |uri|
+  content = get_courses(uri)
+  is_writing.lock
+  File.open('data.html', 'a') do |io|
+    io.puts content
+  end
+  is_writing.unlock
+end
