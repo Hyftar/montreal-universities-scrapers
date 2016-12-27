3faccd1b7178526c4948d4ce13c0f31c7f5b0f81
Simon Landry <simon_landry@ymail.com>
2016-12-27 03:08:04 -0500

    Add HEC programs scraper

---
+++ hec_programs_scraper/parser.rb
@@ -0,0 +1,66 @@
+require 'nokogiri'
+require 'json'
+
+
+class Course
+  attr_reader :title, :number, :url, :credits, :school
+
+  def initialize(title, number, url, credits)
+    @title = title
+    @number = number
+    @url = "https://admission.umontreal.ca/#{url}"
+    @credits = credits
+    @school = 'HEC Montréal'
+  end
+
+  def to_hash
+    {
+      "title" => self.title,
+      "url" => self.url,
+      "number" => self.number,
+      "credits" => self.credits,
+      "school" => self.school,
+    }
+  end
+end
+
+class Program
+  attr_reader :title, :number, :url, :credits, :school
+
+  def initialize(title, number, url, credits)
+    @title = title
+    @number = number
+    @url = url
+    @credits = credits
+    @school = 'HEC Montréal'
+  end
+
+  def to_s
+    "#{self.title}, #{self.number}, #{self.credits}, #{self.url}"
+  end
+
+  def to_hash
+    {
+      "title" => self.title,
+      "url" => self.url,
+      "number" => self.number,
+      "credits" => self.credits,
+      "school" => self.school
+    }
+  end
+end
+courses = Nokogiri::HTML(File.open('data.html', 'r').read)
+  .css('a')
+  .map do |x|
+    Program.new(
+      x.css('h3').text.strip,
+      'none', # Info missing on the website
+      x['href'].strip,
+      x.css('.PF_ProgItemInfoText').text.match(/\d+/).to_s.to_f,
+    )
+  end
+  .map(&:to_hash)
+
+open('programs.json', 'w') do |io|
+  io.puts JSON::generate(courses)
+end
