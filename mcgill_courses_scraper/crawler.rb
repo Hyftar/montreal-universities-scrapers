c23f6b1cd2ec7325a21d79755c6616eb86e5e6ac
Simon Landry <simon_landry@ymail.com>
2016-12-29 20:26:16 -0500

    Add McGill courses scraper

---
+++ mcgill_courses_scraper/crawler.rb
@@ -0,0 +1,52 @@
+require 'net/http'
+require 'nokogiri'
+require 'json'
+require 'parallel'
+require 'irb'
+
+
+class Course
+  attr_reader :title, :number, :url, :credits, :school
+
+  def initialize(title, number, url, credits)
+    @title = title
+    @number = number
+    @url = url
+    @credits = credits
+    @school = 'Université McGill'
+  end
+
+  def to_hash
+    {
+      "title" => self.title,
+      "url" => "http://www.mcgill.ca/#{self.url}",
+      "number" => self.number,
+      "credits" => self.credits,
+      "school" => self.school,
+    }
+  end
+end
+
+def get_uri(index)
+  URI("http://www.mcgill.ca/study/2016-2017/courses/search?search_api_views_fulltext=&sort_by=field_subject_code&page=#{index}")
+end
+
+
+courses = Parallel.map(0..506, in_threads: 30, progress: 'Getting courses info') do |index|
+# courses = (0..506).map do |index|
+  Nokogiri::HTML(Net::HTTP.get(get_uri(index)))
+    .css('div.views-field > h4 > a')
+    .map do |row|
+      binding.irb if (!row.text.match?(/\w+ \w+ (.+) \(\d+\.?\d* c/) || !row.text.match?(/(\w+ \w+)/) || row.text.match?(/\(\d+\.?\d*\)/))
+      Course.new(
+        row.text.match(/\w+ \w+ (.+) \(\d+\.?\d* c/)[1],
+        row.text.match(/(\w+ \w+)/)[1],
+        row['href'],
+        row.text.match(/\((\d+\.?\d*) c/)[1].to_f
+      )
+    end
+end.flatten
+
+File.open('courses.json', 'w') do |io|
+  io.puts JSON.generate(courses.map(&:to_hash))
+end
